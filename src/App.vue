805f6d26f7464188716cadfdf7d4a80ae61f629b
Akira Kanno <teinen.github@gmail.com>
2019-01-17 22:24:19 +0900

    Add base codes

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="main">
+    <vue-scroll-progress></vue-scroll-progress>
+    Scroll Progress bar using Vue.js
+  </div>
+</template>
+
+<script>
+import VueScrollProgress from "./components/VueScrollProgress";
+
+export default {
+  components: {
+    "vue-scroll-progress": VueScrollProgress
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#main {
+  height: 1500px;
+  width: 100vw;
+  padding: 10px 0;
+}
+</style>
---
+++ src/components/VueScrollProgress.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="seekbar" v-bind:style="seekBarStyle"></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      seekBarStyle: {
+        width: "1%"
+      }
+    };
+  },
+  methods: {
+    scrollHandler() {
+      // Get scrolled position
+      const scrolledPer = (window.scrollY / this.calcScreenMaxHeight()) * 100;
+
+      if (scrolledPer <= 1) {
+        this.seekBarStyle.width = "1%";
+      } else {
+        this.seekBarStyle.width = scrolledPer.toString() + "%";
+      }
+    },
+    calcScreenMaxHeight() {
+      const docEl = document.documentElement;
+      return docEl.scrollHeight - docEl.clientHeight;
+    }
+  },
+  mounted() {
+    // add event listner
+    window.addEventListener("scroll", this.scrollHandler);
+  },
+  destroyed() {
+    // remove event listner
+    window.removeEventListener("scroll", this.scrollHandler, false);
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#seekbar {
+  display: block;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  background-color: #42b983;
+  height: 8px;
+  opacity: 0.7;
+}
+</style>
