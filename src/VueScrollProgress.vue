577653f41b792f0f0c537d3648561324b92c2d62
Akira Kanno <teinen.github@gmail.com>
2019-01-17 21:47:17 +0900

    Add base component

---
+++ src/VueScrollProgress.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data() {
+    return {
+      seekBarStyle: {
+        width: "1%"
+      }
+    };
+  },
+  methods: {
+    scrollHandler() {}
+  },
+  mounted() {
+    // add event listner
+    window.addEventListener("scroll", this.scrollHandler);
+  },
+  destroyed() {
+    // remove event listner
+    window.removeEventListener("scroll", this.scrollHandler, false);
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
