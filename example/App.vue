b3bb65aad15790baa9201ed69fd8210808c6a90c
Akira Kanno <teinen.github@gmail.com>
2019-02-16 16:31:06 +0900

    Add sample page

---
+++ example/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="main">
+    <VueSeekBar color="#42b983" opacity="0.9" height="3"></VueSeekBar>
+    Scroll Progress bar using Vue.js
+  </div>
+</template>
+
+<script>
+import VueSeekBar from "../src/VueSeekBar";
+
+export default {
+  components: { VueSeekBar }
+};
+</script>
+
+<style lang="scss" scoped>
+#main {
+  height: 1500px;
+  width: 100vw;
+  padding: 10px 0;
+}
+</style>
