1afd14a838256a93ee2d3161f3fbe9c78445a757
Akira Kanno <teinen.github@gmail.com>
2019-02-17 23:58:54 +0900

    Update sample page

--- example/App.vue
+++ example/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="main">
+    <!-- vue seek bar -->
     <VueSeekBar color="#42b983" opacity="0.9" height="5"></VueSeekBar>
-    Scroll Progress bar using Vue.js
+
+    <h1>Scroll Progress bar using Vue.js</h1>
+
+    <div class="content" v-for="i in 40" :key="i">Please scroll page !!!</div>
+
+    <footer><p>Copyright 2019 Akira Kanno</p></footer>
   </div>
 </template>
 
@@ -17,6 +23,24 @@ export default {
 #main {
   height: 1500px;
   width: 100vw;
-  padding: 10px 0;
+  padding: 10px 0 0 0;
+
+  h1 {
+    text-align: center;
+  }
+
+  .content {
+    text-align: center;
+    margin: 10px 0;
+  }
+
+  footer {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 50px;
+    width: 100vw;
+    border-top: solid 1px #848484;
+  }
 }
 </style>
