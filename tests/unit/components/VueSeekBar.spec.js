9b111b74235a4856d7fafb26b4cc3046352d0624
Akira Kanno <teinen.github@gmail.com>
2019-02-23 21:20:05 +0900

    Add unit testing

--- tests/unit/components/VueSeekBar.spec.js
+++ tests/unit/components/VueSeekBar.spec.js
@@ -9,7 +9,30 @@ const mockProps = {
 
 describe('VueSeekBar.vue', () => {
   it('renders correctly', () => {
-    const wrapper = shallowMount(VueSeekBar, { props: mockProps })
+    const wrapper = shallowMount(VueSeekBar, {
+      propsData: mockProps,
+    })
     expect(wrapper).toMatchSnapshot()
+  })
+
+  it('has correct color props', () => {
+    const wrapper = shallowMount(VueSeekBar, {
+      propsData: mockProps,
+    })
+    expect(wrapper.props().color).toBe('#42b983')
+  })
+
+  it('has correct opacity props', () => {
+    const wrapper = shallowMount(VueSeekBar, {
+      propsData: mockProps,
+    })
+    expect(wrapper.props().opacity).toBe('0.8')
+  })
+
+  it('has correct height props', () => {
+    const wrapper = shallowMount(VueSeekBar, {
+      propsData: mockProps,
+    })
+    expect(wrapper.props().height).toBe('5')
   })
 })
